8c8084ce9b084c8dc5f869dbd477f6ea0267ff72
Arver1 <aactepanov@mail.ru>
2019-01-08 23:18:55 +0300

    added spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './spinner.css';
+
+function Spinner(){
+  return (
+    <div className="cssload-container">
+      <div className="cssload-speeding-wheel"></div>
+    </div>
+  )
+}
+
+export default Spinner;
