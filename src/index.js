755b76b6b9b58e7a0730f7abb785a79857845ad9
Arver1 <aactepanov@mail.ru>
2019-01-08 10:56:13 +0300

    added the markup

--- src/index.js
+++ src/index.js
@@ -1,45 +1,5 @@
-class SwapiService {
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/app';
 
-  _apiBase = 'https://swapi.co/api';
-
-  async getResourse(url){
-    const res = await fetch(`${this._apiBase}${url}`);
-
-    if(!res.ok) {
-      throw new Error(`Could not fetch ${url} received ${res.status}`)
-    }
-    return await res.json();
-  };
-
-  async getAllPeople(){
-    const res = await this.getResourse(`/people/`);
-    return res.results;
-  }
-
-  getPerson(id) {
-    return this.getResourse(`/people/${id}/`)
-  }
-
-  async getAllPlanets(){
-    const res = await this.getResourse(`/planets/`);
-    return res.results;
-  }
-
-  getPlanet(id) {
-    return this.getResourse(`/planets/${id}/`)
-  }
-
-  async getAllStarships(){
-    const res = await this.getResourse(`/starships/`);
-    return res.results;
-  }
-
-  getStarship(id) {
-    return this.getResourse(`/starships/${id}/`)
-  }
-}
-
-const swapi = new SwapiService();
-swapi.getPerson(3).then((p) => {
-  console.log(p.name)
-});
+render(<App />, document.getElementById('root'));
